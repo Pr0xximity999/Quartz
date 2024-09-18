c720ac1f4326080eb163ab33d9eb50a8df7bdd30
Pr0xximity999 <tomsomgamer999@gmail.com>
2024-09-18 17:54:14 +0200

    Quartz sync: Sep 18, 2024, 5:54 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
     pageTitle: "🌙Tom's Docs",
     enableSPA: true,
     enablePopovers: true,
-    analytics: { provider: 'google', tagId: '9710342572'},
+    analytics: { provider: 'google', tagId: 'G-RQ53SEPBR4'},
     locale: "en-US",
     baseUrl: "pr0xximity999.github.io/Quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
